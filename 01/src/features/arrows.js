c024641839fc54a6f1c4099acc99edc4ef0387ce
Philipp Gigler <Siko@Philipps-MacBook-Air-3.local>
2016-10-08 14:59:10 +0200

    Part 3

--- 01/src/features/arrows.js
+++ 01/src/features/arrows.js
@@ -20,9 +20,8 @@ export function es6() {
     list: [1, 2, 3, 4, 5],
     result: 0,
     sum: function() {
-      var that = this
-      this.list.forEach((val) => {
-        that.result += val
+      this.list.map((val) => {
+        this.result += val
       })
     }
   }

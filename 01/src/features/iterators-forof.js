0064bf7fbe84517cf029e4ee12818c62c2571e2b
Martin Gassner <skafield91@web.de>
2016-10-10 16:29:07 +0200

    test iterators-forof

--- 01/src/features/iterators-forof.js
+++ 01/src/features/iterators-forof.js
@@ -1,13 +1,20 @@
 export function es5() {
-  var a = [1, 2, 3, 4, 5]
-  var sum = 0
+    var a = [1, 2, 3, 4, 5]
+    var sum = 0
 
-  for (var i = 0; i < a.length; i += 1) {
-    sum += a[i]
-  }
+    for (var i = 0; i < a.length; i += 1) {
+        sum += a[i]
+    }
 
-  return sum
+    return sum
 }
 
 export function es6() {
+    var a = [1, 2, 3, 4, 5]
+    var sum = 0
+
+    for (let i of a) {
+        sum += i
+    }
+    return sum
 }

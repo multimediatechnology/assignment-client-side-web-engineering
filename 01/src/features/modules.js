ce02a14c1cf6a9d917798d8a233f62fcae9505b4
Martin Gassner <skafield91@web.de>
2016-10-10 16:53:25 +0200

    test modules

--- 01/src/features/modules.js
+++ 01/src/features/modules.js
@@ -1,7 +1,10 @@
+
 export function es5() {
-  var module = require('../utils')
-  return module.uuid()
+    var module = require('../utils')
+    return module.uuid()
 }
 
+import {uuid} from '../utils'
 export function es6() {
+    return uuid()
 }

83c59684b57e7e456d02e865c80fda17e4464e55
Martin Gassner <skafield91@web.de>
2016-10-12 16:04:46 +0200

    test map-set

--- 01/test/features/map-set.js
+++ 01/test/features/map-set.js
@@ -1,32 +1,52 @@
 import {
-  es5,
-  es6
+    es5,
+    es6
 } from '../../src/features/map-set'
 
 export default function() {
-  it('should emulate Map/Set behavior in ES5', () => {
-    var klasses = es5()
-    var mySet = new klasses.MySet()
-    mySet
-      .add(1)
-      .add(2)
-      .add(3)
+    it('should emulate Map/Set behavior in ES5', () => {
+        var klasses = es5()
+        var mySet = new klasses.MySet()
+        mySet
+            .add(1)
+            .add(2)
+            .add(3)
 
-    mySet.has(1).should.be.true
-    mySet.has(2).should.be.true
-    mySet.has(3).should.be.true
-    mySet.has(4).should.be.false
+        mySet.has(1).should.be.true
+        mySet.has(2).should.be.true
+        mySet.has(3).should.be.true
+        mySet.has(4).should.be.false
 
-    var myMap = new klasses.MyMap()
-    myMap.set('Hello', 'World!')
-    myMap.set('second', 2);
-    (function() {
-      myMap.set(1, 2)
-    }).should.throw()
-    myMap.get('Hello').should.eql('World!')
-    myMap.get('second').should.eql(2)
-  })
+        var myMap = new klasses.MyMap()
+        myMap.set('Hello', 'World!')
+        myMap.set('second', 2);
+        (function() {
+            myMap.set(1, 2)
+        }).should.throw()
+        myMap.get('Hello').should.eql('World!')
+        myMap.get('second').should.eql(2)
+    })
 
-  it.skip('should show Map/Set behavior in ES6', () => {
-  })
+    it('should show Map/Set behavior in ES6', () => {
+        const s = new Set()
+        s
+            .add(1)
+            .add(2)
+            .add(3)
+        s.has(1).should.be.true
+        s.has(2).should.be.true
+        s.has(3).should.be.true
+        s.has(4).should.be.false
+
+        const m = new Map()
+        m.set('Hello', 'World!')
+        m.set('second', 2);
+        //bei Map ist das erlaubt
+        // (function() {
+        //     m.set(1, 2)
+        // }).should.throw()
+        m.get('Hello').should.eql('World!')
+        m.get('second').should.eql(2)
+
+    })
 }

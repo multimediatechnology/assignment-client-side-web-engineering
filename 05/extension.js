2622df8296a9bdd6d6df7c9fe620ca5424b933e3
Martin Gassner <skafield91@web.de>
2016-12-01 14:59:59 +0100

    search for gifs from any page

--- 05/extension.js
+++ 05/extension.js
@@ -1,20 +1,15 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var visibleGifs = [];
+var apiKey = 'dc6zaTOxFJmzC';
+var limit = 20;
 
-// This extension demonstrates using chrome.downloads.download() to
-// download URLs.
-
-var allLinks = [];
-var visibleLinks = [];
-
-// Display all visible links.
-function showLinks() {
-    var linksTable = document.getElementById('links');
-    while (linksTable.children.length > 1) {
-        linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
+function showGifs(response) {
+    var responseJson = JSON.parse(response);
+    visibleGifs = responseJson['data'];
+    var gifsTable = document.getElementById('links');
+    while (gifsTable.children.length > 1) {
+        gifsTable.removeChild(gifsTable.children[gifsTable.children.length - 1])
     }
-    for (var i = 0; i < visibleLinks.length; ++i) {
+    for (var i = 0; i < visibleGifs.length; ++i) {
         var row = document.createElement('tr');
         var col0 = document.createElement('td');
         var col1 = document.createElement('td');
@@ -24,7 +19,7 @@ function showLinks() {
         checkbox.type = 'checkbox';
         checkbox.id = 'check' + i;
         col0.appendChild(checkbox);
-        img.src = visibleLinks[i];
+        img.src = visibleGifs[i]['images']['fixed_width_downsampled']['url'];
         img.className += 'giphy';
         img.onclick = downloadGif;
         col1.appendChild(img);
@@ -34,85 +29,53 @@ function showLinks() {
         }
         row.appendChild(col0);
         row.appendChild(col1);
-        linksTable.appendChild(row);
+        gifsTable.appendChild(row);
     }
 }
 
-// Toggle the checked state of all visible links.
 function toggleAll() {
     var checked = document.getElementById('toggle_all').checked;
-    for (var i = 0; i < visibleLinks.length; ++i) {
+    for (var i = 0; i < visibleGifs.length; ++i) {
         document.getElementById('check' + i).checked = checked;
     }
 }
 
-// Download all visible checked links.
-function downloadCheckedLinks() {
-    for (var i = 0; i < visibleLinks.length; ++i) {
+function downloadCheckedGifs() {
+    for (var i = 0; i < visibleGifs.length; ++i) {
         if (document.getElementById('check' + i).checked) {
-            chrome.downloads.download({ url: visibleLinks[i] },
+            chrome.downloads.download({ url: visibleGifs[i] },
                 function(id) {});
         }
     }
     window.close();
 }
 
-// Download all visible checked links.
 function downloadGif(element) {
   chrome.downloads.download({ url: element.target.currentSrc });
 }
 
-// Re-filter allLinks into visibleLinks and reshow visibleLinks.
-function filterLinks() {
+function getGifs() {
     var filterValue = document.getElementById('filter').value;
-    var terms = filterValue.split(' ');
-    visibleLinks = allLinks.filter(function(link) {
-        for (var termI = 0; termI < terms.length; ++termI) {
-            var term = terms[termI];
-            if (term.length != 0) {
-                var expected = (term[0] != '-');
-                if (!expected) {
-                    term = term.substr(1);
-                    if (term.length == 0) {
-                        continue;
-                    }
-                }
-                var found = (-1 !== link.indexOf(term));
-                if (found != expected) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    });
-    showLinks();
+    filterValue = filterValue.replace(' ', '+').replace(/\W+/g, '');
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + filterValue + '&api_key=' + apiKey + '&limit=' + limit;
+    httpGetAsync(url, showGifs);
 }
 
-// Add links to allLinks and visibleLinks, sort and show them.  send_links.js is
-// injected into all frames of the active tab, so this listener may be called
-// multiple times.
-chrome.extension.onRequest.addListener(function(links) {
-    for (var index in links) {
-        allLinks.push(links[index]);
-    }
-    allLinks.sort();
-    visibleLinks = allLinks;
-    showLinks();
-});
 
-// Set up event handlers and inject send_links.js into all frames in the active
-// tab.
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
+
 window.onload = function() {
-    document.getElementById('filter').onkeyup = filterLinks;
+    document.getElementById('filter').onkeyup = getGifs;
     document.getElementById('toggle_all').onchange = toggleAll;
-    document.getElementById('download').onclick = downloadCheckedLinks;
+    document.getElementById('download').onclick = downloadCheckedGifs;
     
-
-    chrome.windows.getCurrent(function(currentWindow) {
-        chrome.tabs.query({ active: true, windowId: currentWindow.id },
-            function(activeTabs) {
-                chrome.tabs.executeScript(
-                    activeTabs[0].id, { file: 'links.js', allFrames: true });
-            });
-    });
 };
--- 05/links.js
+++ 05/links.js
@@ -1,15 +1,6 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var gifs = [].slice.apply(document.getElementsByTagName('img'));
+gifs = gifs.map(function(element) {
 
-// Send back to the popup a sorted deduped list of valid link URLs on this page.
-// The popup injects this script into all frames in the active tab.
-
-var links = [].slice.apply(document.getElementsByTagName('img'));
-links = links.map(function(element) {
-    // Return an anchor's href attribute, stripping any URL fragment (hash '#').
-    // If the html specifies a relative path, chrome converts it to an absolute
-    // URL.
     if (element.className === 'grid_12' || element.className === 'color') {
         return '';
     } else {
@@ -27,18 +18,17 @@ links = links.map(function(element) {
     return '';
 });
 
-links.sort();
+gifs.sort();
 
-// Remove duplicates and invalid URLs.
 var kBadPrefix = 'javascript';
-for (var i = 0; i < links.length;) {
-    if (((i > 0) && (links[i] == links[i - 1])) ||
-        (links[i] == '') ||
-        (kBadPrefix == links[i].toLowerCase().substr(0, kBadPrefix.length))) {
-        links.splice(i, 1);
+for (var i = 0; i < gifs.length;) {
+    if (((i > 0) && (gifs[i] == gifs[i - 1])) ||
+        (gifs[i] == '') ||
+        (kBadPrefix == gifs[i].toLowerCase().substr(0, kBadPrefix.length))) {
+        gifs.splice(i, 1);
     } else {
         ++i;
     }
 }
 
-chrome.extension.sendRequest(links);
+chrome.extension.sendRequest(gifs);

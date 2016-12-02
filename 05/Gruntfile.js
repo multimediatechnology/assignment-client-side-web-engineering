b3c0166b3d01183d8941c94464655ab19c62510e
Martin Gassner <skafield91@web.de>
2016-12-02 14:41:26 +0100

    refactor

--- 05/Gruntfile.js
+++ 05/Gruntfile.js
@@ -5,93 +5,109 @@ const serveStatic = require('serve-static')
 
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt)
-  require('time-grunt')(grunt)
+    require('load-grunt-tasks')(grunt)
+    require('time-grunt')(grunt)
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    browserify: {
-      watch: {
-        files: {
-          './dist/extension.js': ['./src/extension.js'],
-          './dist/gifs.js': ['./src/gifs.js']
+        browserify: {
+            watch: {
+                files: {
+                    './dist/extension.js': ['./src/extension.js']
+                },
+                options: {
+                    transform: ['hbsfy', 'babelify']
+                }
+            },
+            dist: {
+                files: {
+                    './dist/extension.js': ['./src/extension.js']
+                },
+                options: {
+                    transform: ['hbsfy', 'babelify']
+                }
+            }
         },
-        options: {
-          transform: ['hbsfy', 'babelify']
-        }
-      },
-      dist: {
-        files: {
-          './dist/extension.js': ['./src/extension.js'],
-          './dist/gifs.js': ['./src/gifs.js']
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    './dist/extension.js': ['./src/extension.js']
+                }
+            }
         },
-        options: {
-          transform: ['hbsfy', 'babelify', 'uglifyify']
+
+        clean: {
+            dist: ['./dist']
+        },
+
+
+        copy: {
+            dist: {
+                files: [
+                    // styles
+                    {
+                        expand: true,
+                        cwd: 'src/assets/styles',
+                        src: '*.css',
+                        dest: './dist/assets/styles'
+                    },
+                    // icons
+                    {
+                        expand: true,
+                        cwd: './src/assets/icons',
+                        src: '**/*',
+                        dest: './dist/assets/icons'
+                    },
+                    // html
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: '*.html',
+                        dest: './dist/'
+                    },
+                    // manifest
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: 'manifest.json',
+                        dest: './dist/'
+                    },
+                    // jquery
+                    {
+                        expand: true,
+                        cwd: './node_modules/jquery/dist/',
+                        src: 'jquery.min.js',
+                        dest: './dist/lib/'
+                    },
+                ]
+
+            }
+        },
+
+        watch: {
+            static: {
+                files: ['./src/**/*.css', './src/*.html'],
+                tasks: ['copy'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['./src/**/*.js'],
+                tasks: ['browserify:watch'],
+                options: {
+                    livereload: true
+                }
+            }
         }
-      }
-    },
+    })
 
-    clean: {
-      dist: ['./dist']
-    },
-
-
-    copy: {
-      dist: {
-        files: [
-          // styles
-          {
-            expand: true,
-            cwd: 'src/assets/styles',
-            src: '*.css',
-            dest: './dist/assets/styles'
-          },
-          // icons
-          {
-            expand: true,
-            cwd: './src/assets/icons',
-            src: '**/*',
-            dest: './dist/assets/icons'
-          },
-          // html
-          {
-            expand: true,
-            cwd: 'src',
-            src: '*.html',
-            dest: './dist/'
-          },
-          // manifest
-          {
-            expand: true,
-            cwd: 'src',
-            src: 'manifest.json',
-            dest: './dist/'
-          },
-        ]
-
-      }
-    },
-
-    watch: {
-      static: {
-        files: ['./src/**/*.css', './src/*.html'],
-        tasks: ['copy'],
-        options: {
-          livereload: true
-        }
-      },
-      js: {
-        files: ['./src/**/*.js'],
-        tasks: ['browserify:watch'],
-        options: {
-          livereload: true
-        }
-      }
-    }
-  })
-
-  grunt.registerTask('default', ['clean', 'copy', 'browserify:dist'])
-  grunt.registerTask('start', ['default', 'watch'])
+    grunt.registerTask('default', ['clean', 'copy', 'browserify:dist', 'uglify'])
+    grunt.registerTask('start', ['default', 'watch'])
 
 }
--- 05/src/extension.js
+++ 05/src/extension.js
@@ -5,65 +5,42 @@ var limit = 20;
 function showGifs(response) {
     var responseJson = JSON.parse(response);
     visibleGifs = responseJson['data'];
-    var gifsTable = document.getElementById('links');
-    while (gifsTable.children.length > 1) {
-        gifsTable.removeChild(gifsTable.children[gifsTable.children.length - 1])
-    }
+    $('#gifs').empty();
     for (var i = 0; i < visibleGifs.length; ++i) {
-        var row = document.createElement('tr');
-        var col0 = document.createElement('td');
-        var col1 = document.createElement('td');
-        var img = document.createElement('img');
-        var checkbox = document.createElement('input');
-        checkbox.checked = false;
-        checkbox.type = 'checkbox';
-        checkbox.id = 'check' + i;
-        col0.appendChild(checkbox);
-        img.src = visibleGifs[i]['images']['fixed_width_downsampled']['url'];
-        img.className += 'giphy';
-        img.onclick = downloadGif;
-        col1.appendChild(img);
-        col1.style.whiteSpace = 'nowrap';
-        col1.onclick = function() {
-            checkbox.checked = !checkbox.checked;
-        }
-        row.appendChild(col0);
-        row.appendChild(col1);
-        gifsTable.appendChild(row);
+        var img = $('<img/>')
+            .attr('src', visibleGifs[i]['images']['fixed_width_downsampled']['url'])
+            .click(img, function() {
+                var src = $(this).attr('src')
+                downloadGif(src)
+            })
+            .appendTo('#gifs')
+            .wrap('<li class="giphy"></li>')
+
     }
 }
 
-function toggleAll() {
-    var checked = document.getElementById('toggle_all').checked;
+function downloadAll() {
     for (var i = 0; i < visibleGifs.length; ++i) {
-        document.getElementById('check' + i).checked = checked;
-    }
-}
-
-function downloadCheckedGifs() {
-    for (var i = 0; i < visibleGifs.length; ++i) {
-        if (document.getElementById('check' + i).checked) {
-            chrome.downloads.download({ url: visibleGifs[i] },
-                function(id) {});
-        }
+        chrome.downloads.download({
+            url: visibleGifs[i]['images']['fixed_width_downsampled']['url']
+        });
     }
     window.close();
 }
 
 function downloadGif(element) {
-  chrome.downloads.download({ url: element.target.currentSrc });
+    chrome.downloads.download({ url: element });
 }
 
 function getGifs() {
-    var filterValue = document.getElementById('filter').value;
+    var filterValue = $('#filter').val();
     filterValue = filterValue.replace(' ', '+').replace(/\W+/g, '');
     var url = 'http://api.giphy.com/v1/gifs/search?q=' + filterValue + '&api_key=' + apiKey + '&limit=' + limit;
     httpGetAsync(url, showGifs);
 }
 
 
-function httpGetAsync(theUrl, callback)
-{
+function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
@@ -73,9 +50,11 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
-window.onload = function() {
-    document.getElementById('filter').onkeyup = getGifs;
-    document.getElementById('toggle_all').onchange = toggleAll;
-    document.getElementById('download').onclick = downloadCheckedGifs;
-    
-};
+$(document).ready(function() {
+    $('#filter').keyup(function(event) {
+        getGifs();
+    });
+    $('#download').click(function(event) {
+        downloadAll();
+    });
+});
